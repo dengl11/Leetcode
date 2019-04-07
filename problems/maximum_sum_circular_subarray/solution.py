b7ef40d640b10c90d279b2bea556ab756a7eeadb
Li Deng <ldeng1314@gmail.com>
2019-04-07 15:47:19 +0000

    Sync LeetCode submission - Maximum Sum Circular Subarray (python)

--- problems/maximum_sum_circular_subarray/solution.py
+++ problems/maximum_sum_circular_subarray/solution.py
@@ -1,25 +1,14 @@
-class Solution:
-    def maxSubarraySumCircular(self, A: List[int]) -> int:
-        ans = curr = A[0]
-        n = len(A)
-        for x in A[1:]:
-            curr = x + max(curr, 0)
-            ans = max(ans, curr)
-        postfix = [0] * (n - 1)
-        prefix = postfix[:]
-        presum = prefix[0] = A[0]
-        for i in range(1, n-1):
-            presum += A[i]
-            prefix[i] = max(presum, prefix[i-1])
-        postfix[0] = postsum = A[-1]
-        for i in range(1, n-1):
-            postsum += A[~i]
-            postfix[i] = max(postsum, postfix[i-1])
-        for i in range(1, n):
-            ans = max(ans, prefix[i-1] + postfix[n - i - 1])
-        return ans
-            
-            
-        
-            
-            
+class Solution(object):
+    def maxSubarraySumCircular(self, A):
+        def kadane(gen):
+            # Maximum non-empty subarray sum
+            ans = cur = None
+            for x in gen:
+                cur = x + max(cur, 0)
+                ans = max(ans, cur)
+            return ans
+
+        S = sum(A)
+        ans1 = kadane(iter(A))
+        ans2 = S + kadane(-A[i] for i in xrange(1, len(A)-1))
+        return max(ans1, ans2)
