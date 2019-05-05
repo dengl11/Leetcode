6a366f3cb9863f5ddd796b5c8eefbe7fdeefcf7b
Li Deng <ldeng1314@gmail.com>
2019-05-05 20:44:17 +0000

    Sync LeetCode submission - Tallest Billboard (python3)

--- problems/tallest_billboard/solution.py
+++ problems/tallest_billboard/solution.py
@@ -1,14 +1,10 @@
 from collections import defaultdict
-class Solution(object):
-    def tallestBillboard(self, rods):
-        """
-        :type rods: List[int]
-        :rtype: int
-        """
+class Solution:
+    def tallestBillboard(self, rods: List[int]) -> int:
         dp = defaultdict(int)
         dp[0] = 0
         for n in rods:
-            for k, v in dp.items():
+            for k, v in list(dp.items()):
                 dp[k+n] = max(dp[k+n], v)
                 dp[abs(k-n)] = max(dp[abs(k-n)], min(v+k, v + n))
         return dp[0]
