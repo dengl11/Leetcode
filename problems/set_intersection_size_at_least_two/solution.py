94f16a20313b2e94ddad495ffcfef19a28983ac4
Li Deng <ldeng1314@gmail.com>
2019-05-24 15:54:12 +0000

    Sync LeetCode submission - Set Intersection Size At Least Two (python3)

---
+++ problems/set_intersection_size_at_least_two/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def intersectionSizeTwo(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key = lambda x:x[1]) # sort by end-point
+        ans = 0
+        pre = []
+        for (s, t) in intervals:
+            if not pre or pre[1] < s:
+                ans += 2
+                pre = [t-1, t]
+            else:
+                if pre[0] < s:
+                    pre = [pre[1], t]
+                    ans += 1
+        return ans
+                    
+                
+                
+            
+        
