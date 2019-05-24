58a104b22e43966a05a71dfc22e9b3111cc04dd4
Li Deng <ldeng1314@gmail.com>
2019-05-24 04:43:35 +0000

    Sync LeetCode submission - Couples Holding Hands (python)

--- problems/couples_holding_hands/solution.py
+++ problems/couples_holding_hands/solution.py
@@ -1,16 +1,33 @@
+class DUS:
+    def __init__(self, n):  # disjoint union set 模板，请参考花花酱视频
+        self.parent = [i for i in range(n)]
+        self.counts = 0
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+    def merge(self, x, y):
+        px = self.find(x)
+        py = self.find(y)
+        if px != py:  # 优化：记录多少次merge操作。 如果不记录counts的话，需要额外的O(N)复杂度来计算
+            self.counts += 1
+        self.parent[px] = py
+        
+        
 class Solution:
-    def minSwapsCouples(self, row: List[int]) -> int:
-        n = len(row) // 2
-        def partner(i):
-            return i-1 if i%2 else i+1
+    def minSwapsCouples(self, row):
+        """
+        :type row: List[int]
+        :rtype: int
+        """
+        n = len(row)//2
+        dus = DUS(n)
+        
+        for i in range(n):
+            x = row[2*i] // 2
+            y = row[2*i+1] // 2
             
-        pos = {x:i for (i, x) in enumerate(row)}
-        ans = 0
-        for i in range(0, n*2, 2):
-            p = partner(row[i])
-            if row[i+1] != p:
-                j = pos[p]
-                pos[row[i+1]] = j
-                row[i+1], row[j] = row[j], row[i+1]
-                ans += 1
-        return ans
+            if x!=y:
+                dus.merge(x, y)
+        
+        return dus.counts
