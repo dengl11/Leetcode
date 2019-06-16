996c3cb16b771747201fb2287ea9844c7e2b968b
Li Deng <ldeng1314@gmail.com>
2019-06-16 22:56:06 +0000

    Sync LeetCode submission - Exam Room (python3)

--- problems/exam_room/solution.py
+++ problems/exam_room/solution.py
@@ -1,52 +1,27 @@
-from heapq import heappush, heappop
+from bisect import insort
 class ExamRoom:
 
     def __init__(self, N: int):
-        self.slots = [(-N, 0, 0, N - 1)]
         self.N = N
-        self.starts = {0: (0, N-1)} # {start: (i, j)}
-        self.ends = {N-1: (0, N-1)} # {ends: (i, j)}
+        self.L = []
         
 
     def seat(self) -> int:
-        # print(self.slots)
-        while self.slots:
-            _, _, i, j = heappop(self.slots)
-            if i not in self.starts or j != self.starts[i][-1]: continue
-            if i == 0: seat = 0
-            elif j == self.N -1: seat = j
-            else: seat = (i + j) // 2
-                
-            del self.starts[i]
-            del self.ends[j]
-            if seat > i:
-                self.push(i, seat - 1)
-            if seat < j:
-                self.push(seat+1, j)
-            return seat
+        ans = 0
+        if self.L: 
+            dist = self.L[0]
+            for i, j in zip(self.L, self.L[1:]):
+                if (j-i) // 2 > dist:
+                    dist = (j-i)//2
+                    ans = (j+i)//2
+            if self.N-1-self.L[-1]> dist:
+                ans = self.N-1
+        insort(self.L, ans)
+        return ans
+            
         
-    def push(self, left, right):
-        d = (right + left) // 2 - left
-        if left == 0:
-            d = right
-        if right == self.N-1:
-            d = self.N-1 - left
-        heappush(self.slots, (-d, left, left, right))  
-        self.starts[left] = (left, right)
-        self.ends[right] = (left, right)
-        
-        
-
     def leave(self, p: int) -> None:
-        left = right = p
-        if p > 0 and (p-1) in self.ends:
-            left = self.ends[p-1][0]
-            del self.ends[p-1]
-        if p < self.N-1 and (p + 1) in self.starts:
-            right = self.starts[p+1][1]
-            del self.starts[p+1]
-        self.push(left, right)
-        
+        self.L.remove(p)
 
 
 # Your ExamRoom object will be instantiated and called as such:
