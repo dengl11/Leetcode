a663910669ef2cf3cac062025c55597386b09cb5
Li Deng <ldeng1314@gmail.com>
2019-06-21 14:57:04 +0000

    Sync LeetCode submission - Minimum Cost to Hire K Workers (python3)

--- problems/minimum_cost_to_hire_k_workers/solution.py
+++ problems/minimum_cost_to_hire_k_workers/solution.py
@@ -1,19 +1,16 @@
-from heapq import heappop,heappush
+from heapq import heappush, heappop
 class Solution:
     def mincostToHireWorkers(self, quality: List[int], wage: List[int], K: int) -> float:
-        pairs = sorted((w/q, q) for w,q in zip(wage, quality))
-        wqs = [p[0] for p in pairs]
-        qs = [p[1] for p in pairs]
-        q = []
-        for j in range(K):
-            heappush(q, -qs[j])
-        accQs = sum(qs[:K])
-        ans = accQs * wqs[K-1]
-        for k in range(K, len(qs)):
-            accQs += qs[k] + heappop(q)
-            heappush(q, -qs[k])
-            ans = min(ans, wqs[k] * accQs)
+        workers = [(q, w, w/q) for (w, q) in zip(wage, quality)]
+        workers.sort()
+        pq = []
+        ans = sum(x[0] for x in workers[:K]) * max(x[-1] for x in workers[:K])
+        Q = 0
+        for q, w, ratio in workers:
+            heappush(pq, (-ratio, -q))
+            Q += q
+            if len(pq) > K:
+                nr, nq = heappop(pq)
+                Q += nq
+                ans = min(ans, Q * (-pq[0][0]))
         return ans
-        
-        
-        
