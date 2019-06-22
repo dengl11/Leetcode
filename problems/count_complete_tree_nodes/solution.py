9618719e2f0d95abc7e31f4b4c06890942b5f50f
Li Deng <ldeng1314@gmail.com>
2019-06-22 14:31:36 +0000

    Sync LeetCode submission - Count Complete Tree Nodes (python3)

--- problems/count_complete_tree_nodes/solution.py
+++ problems/count_complete_tree_nodes/solution.py
@@ -5,8 +5,15 @@
 #         self.left = None
 #         self.right = None
 
+def depth(node):
+    if not node: return 0
+    return 1 + depth(node.left)
+        
 class Solution:
     def countNodes(self, root: TreeNode) -> int:
         if not root: return 0
-        return 1 + self.countNodes(root.left) + self.countNodes(root.right)
+        dl, dr = depth(root.left), depth(root.right)
+        if dl == dr:
+            return (1 if dl == 0 else (1 << dl)) + self.countNodes(root.right)
+        return (1 if dr == 0 else (1 << dr)) + self.countNodes(root.left)
         
