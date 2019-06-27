eb51cae71472ff5ea7f69c97984672de9e0ba44f
Li Deng <ldeng1314@gmail.com>
2019-06-27 15:46:47 +0000

    Sync LeetCode submission - Longest Increasing Subsequence (python)

---
+++ problems/longest_increasing_subsequence/solution.py
@@ -0,0 +1,16 @@
+from bisect import bisect_left as bl
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        dp = [0] * len(nums)
+        L = 0
+        for i, x in enumerate(nums):
+            j = bl(dp, x, lo=0, hi = max(L, 0))
+            # print(dp, x, j)
+            if j == L:
+                L += 1
+            dp[j] = x
+        return L
