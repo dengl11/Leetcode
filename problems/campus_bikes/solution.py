0a0877b8d7e2e493ac4d52225f07d4619a29149e
Li Deng <ldeng1314@gmail.com>
2019-07-10 14:10:08 +0000

    Sync LeetCode submission - Campus Bikes (python)

--- problems/campus_bikes/solution.py
+++ problems/campus_bikes/solution.py
@@ -1,24 +1,29 @@
 from heapq import heappop, heappush
-class Solution:
-    def assignBikes(self, workers: List[List[int]], bikes: List[List[int]]) -> List[int]:
-        n = len(workers)
+class Solution(object):
+    def assignBikes(self, workers, bikes):
+        """
+        :type workers: List[List[int]]
+        :type bikes: List[List[int]]
+        :rtype: int
+        """
         def dist(w, b):
-            return abs(w[0]-b[0]) + abs(w[1]-b[1])
-        todos = [None for _ in range(len(bikes))]
-        for j, b in enumerate(bikes):
-            todos[j] = sorted((dist(b, workers[i]), i, j) for i in range(n))
-        starts = [0] * len(bikes)
+            return abs(w[0] - b[0]) + abs(w[1] - b[1])
         q = []
-        for i in range(len(bikes)):
-            heappush(q, todos[i][0])
-        ans = [-1] * n
-        while n:
-            dist, iw, ib = heappop(q)
-            if ans[iw] >= 0:
-                if starts[ib] + 1 < len(workers):
-                    starts[ib] += 1
-                    heappush(q, todos[ib][starts[ib]])
+        W = []
+        for ib, b in enumerate(bikes):
+            curr = sorted([(dist(w, b), iw, ib) for iw, w in enumerate(workers)], reverse=True)
+            heappush(q, curr.pop())
+            W.append(curr)
+            
+        ans = dict()
+        while len(ans) < len(workers):
+            d, iw, ib = heappop(q)
+            if iw not in ans:
+                ans[iw]  = ib
             else:
-                ans[iw] = ib
-                n -= 1
-        return ans
+                heappush(q, W[ib].pop())
+        return [ans[i] for i in range(len(workers))]
+                
+                
+            
+            
