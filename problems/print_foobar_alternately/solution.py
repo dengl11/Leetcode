9c1bee57af45a91a0650c26c40261fa23b811628
Li Deng <ldeng1314@gmail.com>
2019-07-14 22:49:29 +0000

    Sync LeetCode submission - Print FooBar Alternately (python3)

---
+++ problems/print_foobar_alternately/solution.py
@@ -0,0 +1,22 @@
+from threading import Semaphore
+class FooBar:
+    def __init__(self, n):
+        self.n = n
+        self.sem_foo = Semaphore(1)
+        self.sem_bar = Semaphore(0)
+
+
+    def foo(self, printFoo: 'Callable[[], None]') -> None:
+        
+        for i in range(self.n):
+            self.sem_foo.acquire()
+            # printFoo() outputs "foo". Do not change or remove this line.
+            printFoo()
+            self.sem_bar.release()
+
+
+    def bar(self, printBar: 'Callable[[], None]') -> None:
+        for i in range(self.n):
+            self.sem_bar.acquire()
+            printBar()
+            self.sem_foo.release()
