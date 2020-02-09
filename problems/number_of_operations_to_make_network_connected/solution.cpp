3c4d0399344071398aae7c30c7b8955e9841536f
Li Deng <ldeng1314@gmail.com>
2020-02-09 21:41:48 +0000

    Sync LeetCode submission - Number of Operations to Make Network Connected (cpp)

--- problems/number_of_operations_to_make_network_connected/solution.cpp
+++ problems/number_of_operations_to_make_network_connected/solution.cpp
@@ -1,61 +1,29 @@
 #include <algorithm>
 
-class UnionFind {
-public:
-    UnionFind(int n, vector<vector<int>>& connections) :n_(n) {
-        roots.reserve(n);
-        rank.reserve(n);
-        fill(rank.begin(), rank.end(), 1);
-        for (int i = 0; i < n; i ++) {
-            roots[i] = i;
-        }
-        nC = n;
-        for (vector<int> pair : connections) {
-            union_two(pair[0], pair[1]);
-        }
-    }   
-    
-    int find(int i) {
-        while (roots[i] != i) {
-            i = roots[i];
-        }
-        return i;
-    }
-    
-    void union_two(int i, int j) {
-        int root_i = find(i);
-        int root_j = find(j);
-        if (root_i == root_j) return;
-        nC -= 1;
-        if (rank[root_i] < rank[root_j]) {
-            int tmp = root_i;
-            root_i = root_j;
-            root_j = tmp;
-        }
-        roots[root_i] = root_j;
-        if (rank[root_i] == rank[root_j])
-            rank[root_i] += 1;
-            
-    }
-    
-    int nComponents() {
-        return nC;
-    }
-    
-    private:
-    int n_;
-    vector<int> roots;
-    vector<int> rank;
-    int nC;
-    
-};
-
 class Solution {
 public:
     int makeConnected(int n, vector<vector<int>>& connections) {
         if (connections.size() < n-1) return -1;
-        UnionFind uf = UnionFind(n, connections);
-        return uf.nComponents() - 1;
-        
+        vector<vector<int>> nbrs(n);
+        vector<int> seen(n, 0);
+        for (const vector<int>& edge : connections) {
+            nbrs[edge[0]].push_back(edge[1]);
+            nbrs[edge[1]].push_back(edge[0]);
+        }
+        std::fill(seen.begin(), seen.end(), 0);
+        std::function<int(int)> dfs;
+        dfs =  [&](int i) -> int {
+        if (seen[i]) return 0;
+        seen[i] = 1;
+        for (int j : nbrs[i]) dfs(j);
+        return 1;
+    };
+        int ans = 0;
+        for (int i = 0; i < n; i ++) {
+            ans += dfs(i);
+        }
+        return ans - 1;
     }
+
+    
 };
