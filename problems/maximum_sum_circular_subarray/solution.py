02380fa0309fbbcd73eed70be439c3d7eedb808c
Li Deng <ldeng1314@gmail.com>
2020-05-16 00:24:28 +0000

    Sync LeetCode submission - Maximum Sum Circular Subarray (python3)

--- problems/maximum_sum_circular_subarray/solution.py
+++ problems/maximum_sum_circular_subarray/solution.py
@@ -1,14 +1,19 @@
-class Solution(object):
-    def maxSubarraySumCircular(self, A):
-        def kadane(gen):
-            # Maximum non-empty subarray sum
-            ans = cur = None
-            for x in gen:
-                cur = x + max(cur, 0)
-                ans = max(ans, cur)
-            return ans
-
-        S = sum(A)
-        ans1 = kadane(iter(A))
-        ans2 = S + kadane(-A[i] for i in xrange(1, len(A)-1))
-        return max(ans1, ans2)
+from heapq import heappop, heappush, nsmallest
+class Solution:
+    def maxSubarraySumCircular(self, A: List[int]) -> int:
+        aa = [0] + A + A
+        n = len(A)
+        for i in range(1, n * 2+1):
+            aa[i] += aa[i-1]
+        h = []
+        ans = A[0]
+        for i, x in enumerate(aa):
+            if h:
+                while h and i - h[0][1] > n:
+                    heappop(h)
+                if h:
+                    ans = max(ans, x - h[0][0])
+            heappush(h, (x, i))
+        return ans
+            
+                
